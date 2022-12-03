5a263c7cc24f91f7a3587c3097a3416f18981413
proxyprince <fio1120@icloud.com>
2022-12-02 20:09:30 -0500

    project changes

---
+++ music_app/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+from music_app.models import Artist, Song
+from music_app.forms import ArtistForm, SongForm
+from django.urls import reverse_lazy
+# Create your views here.
+
+
+class LandingPageView(TemplateView):
+    template_name = 'home.html'
+
+
+class ArtistListView(ListView):
+    model = Artist
+    context_object_name = 'artists'
+    template_name = 'list_artist.html'
+
+
+class ArtistCreateView(CreateView):
+    model = 'Artist'
+    form_class = ArtistForm
+    template_name = 'add_artist.html'
+    success_url = reverse_lazy('artists')
+
+
+class ArtistUpdateView(UpdateView):
+    model = Artist
+    form_class = ArtistForm
+    template_name = 'edit_artist.html'
+    success_url = reverse_lazy('artists')
+
+
+def deleteArtist(request, pk):
+    artist = Artist.objects.get(id=pk)
+    artist.delete()
+    return redirect('/artists')
+
+class SongListView(ListView):
+    model = Song
+    context_object_name = 'songs'
+    template_name = 'list_songs.html'
+
+
+class SongCreateView(CreateView):
+    model = Song
+    form_class = SongForm
+    template_name = 'add_song.html'
+    success_url = reverse_lazy('songs')
+
+
+class SongUpdateView(UpdateView):
+    model = Song
+    form_class = SongForm
+    template_name = 'edit_song.html'
+    success_url = reverse_lazy('songs')
+
+
+def deleteSong(request, pk):
+    data = get_object_or_404(Song, id=pk)
+    data.delete()
+    return redirect('/songs')
---
+++ music_genie/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from music_app.views import LandingPageView, ArtistListView, ArtistCreateView, SongCreateView, SongListView, SongUpdateView, deleteSong
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='home'),
+    path('artists', ArtistListView.as_view(), name='artists'),
+    path('add_artists', ArtistCreateView.as_view(), name='add_artist'),
+    path('songs', SongListView.as_view(), name='songs'),
+    path('add_song', SongCreateView.as_view(), name='add_song'),
+    path('song-details/<int:pk>', SongUpdateView.as_view()),
+    path('song-delete/<int:pk>', deleteSong),
+]
